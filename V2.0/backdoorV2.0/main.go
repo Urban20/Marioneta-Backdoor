c328b4fa11a27f2eda5d19998f4ce5be239f0f66
Urb@n <matiasurbaneja@hotmail.com.ar>
2025-06-22 20:44:36 -0300

    agrego aclaraciones en main del backdoor

--- V2.0/backdoorV2.0/main.go
+++ V2.0/backdoorV2.0/main.go
@@ -1,10 +1,19 @@
 package main
 
 /*
-1. fucnion mover al startup
-2. implementar conexiones
-3. implementar ejecucion de comandos y comunicacion
-4 . screenshot y envio codificado
+ACLARACION MUY IMPORTANTE, LEER CON ATENCION:
+
+Este programa NO fue hecho con fines maliciosos y NO me hago responsable de su mal uso.
+Simplemente es una utilidad para manipular computadoras fuera del alcance fisico y
+dentro de la red (NO esta pensado ni programado para manipular computadoras fuera de la red)
+(hacer esto fuera de la red es extremadamente peligroso si no se sabe lo que se esta haciendo)
+
+Este programa actua como servidor y el cliente es el usuario que la manipulara
+Se debe asegurar que el backdoor tenga el firewall habilitado en el puerto correspondiente (9999 por defecto)
+
+NOTA: el programa solo esta pensado para entornos WINDOWS, puede que no funcionar en otros sistemas operativos
+
+Autor : Urb@n - https//:www.github.com/Urban20/ - "estamos hack"
 */
 
 import (
